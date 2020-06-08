34ab8e18180edeb0cf687ded9ce0e15bf92c1b7f
Letpmartins <leticiapmartins08@gmail.com>
2020-06-08 15:11:42 +0000

    correcoes

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import './style.css';
 
 export default function Container(props){
-    return <section className="container-principal"></section>;
+    return <section className="container-principal">
+        {props.children}
+    </section>;
 }
