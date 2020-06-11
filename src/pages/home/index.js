7466bd41f146d875394c51148462021f883e6255
Letpmartins <leticiapmartins08@gmail.com>
2020-06-11 18:13:32 +0000

    usestate

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,26 @@
 import React from 'react';
 import '../../Global.css';
-import Card from '../../components/card';
-import Pizza from '../../assets/img/Pizza.jpg';
+// import Card from '../../components/card';
+// import Pizza from '../../assets/img/Pizza.jpg';
 import Header from '../../components/header';
-import Botao from '../../components/botao';
+// import Botao from '../../components/botao';
 import Item from '../../components/item';
 import Container from '../../components/container';
+import { useState } from 'react';
 
 function Home() {
+    const [listaItens, setListaItens] = useState([<Item/>, <Item/>, <Item/>]);
+    function addDoacao(){
+        setListaItens([...listaItens, <Item/>])
+    }
   return (
     <div className="App">
       <Header />
-      <h1>Hello World</h1>
-      <Card />
-      <Card />
-      <img src={Pizza} alt="foto da pizza"/>
-      <Botao texto="Confirmar" cor="verde"/>
-      <Botao texto="Excluir"/>
-      <Container>
-        <Item titulo="Alimentos" autor="Letícia" 
-        descricao="teste teste teste teste teste teste 
-        teste teste teste" />
-        <Item titulo="Alimentos" autor="Letícia" 
-        descricao="teste teste teste teste teste teste 
-        teste teste teste" />
-        <Item titulo="Alimentos" autor="Letícia" 
-        descricao="teste teste teste teste teste teste 
-        teste teste teste" />
-        <Item titulo="Alimentos" autor="Letícia" 
-        descricao="teste teste teste teste teste teste 
-        teste teste teste" />
-        <Item titulo="Alimentos" autor="Letícia" 
-        descricao="teste teste teste teste teste teste 
-        teste teste teste" />
-      </Container>
+      <div>
+          <h1>Lista de Doações</h1>
+          <button onClick={addDoacao}>Criar Doação</button>
+      </div>
+      <Container>{listaItens}      </Container>
     </div>
   );
 }
